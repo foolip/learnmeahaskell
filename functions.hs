20b04f1946f11159b07eca318c4c7adbb8b3b7f6
Philip Jägenstedt <philip@foolip.org>
2014-09-01 00:25:47 +0200

    Higher order functions: http://learnyouahaskell.com/higher-order-functions

--- functions.hs
+++ functions.hs
@@ -6,19 +6,19 @@ forEach _ [] = []
 forEach f (x:xs) = f x : forEach f xs
 
 applyTwice :: (a -> a) -> a -> a
-applyTwice f x = f (f x)
+applyTwice f = f . f
 
 applyThrice :: (a -> a) -> a -> a
-applyThrice f x = f (f (f x))
+applyThrice f = f . f . f
 
 applyFourTimes :: (a -> a) -> a -> a
 applyFourTimes = applyTwice applyTwice
 
 applySevenTimes :: (a -> a) -> a -> a
-applySevenTimes f x = applyFourTimes f (applyThrice f x)
+applySevenTimes f = applyFourTimes f . applyThrice f
 
 applyTwelveTimes :: (a -> a) -> a -> a
-applyTwelveTimes f = applyFourTimes (applyThrice f)
+applyTwelveTimes = applyFourTimes . applyThrice
 
 applyTimes :: (Integral t) => t -> (a -> a) -> a -> a
 applyTimes 1 f x = f x
@@ -26,3 +26,9 @@ applyTimes n f x | n > 1 = applyTimes (n - 1) f (f x)
 
 mul3 :: (Num a) => a -> a
 mul3 x = applyTimes 3 (x+) 0
+
+sum' :: (Num a) => [a] -> a
+sum' = foldl (\sum x -> sum + x) 0
+
+max' :: (Ord a) => [a] -> a
+max' = foldl1 (\x y -> if x > y then x else y)
